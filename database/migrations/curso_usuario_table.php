6b430df182b12abfc3ffb779f143fab427d3a42a
Kyt Dzz <dzzkyt@gmail.com>
2026-04-11 03:24:14 +0100

    Update curso_usuario_table.php

--- database/migrations/curso_usuario_table.php
+++ database/migrations/curso_usuario_table.php
@@ -1,3 +1,7 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
 Schema::create('curso_usuario', function (Blueprint $table) {
     $table->foreignId('id_curso')
         ->constrained('cursos')
